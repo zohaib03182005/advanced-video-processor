97beb11c0b4b4f52735a170cc316ef142d452f4a
zohaib03182005 <zohaib03182005@gmail.com>
2026-09-10 11:25:50 +0500

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from '../App-Advanced.jsx.jsx';
 
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
